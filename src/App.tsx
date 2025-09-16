e9de2abac442b25a490b0852b00e0fbee91889b2
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-16 19:59:14 -0300

    feat(routing): setup basic app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Navigate } from "react-router-dom";
+import { Home } from "@pages/Home";
+import { AppLayout } from "@components/AppLayout";
+import { Paths } from "@routing/paths";
+import { RoutesWithNotFound } from "@routing/RoutesWithNotFound";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <AppLayout>
+        <RoutesWithNotFound>
+          <Route path="/" element={<Navigate to={Paths.Home} />} />
+          <Route path={Paths.Home} element={<Home />} />
+        </RoutesWithNotFound>
+      </AppLayout>
+    </div>
+  );
 }
 
 export default App;
