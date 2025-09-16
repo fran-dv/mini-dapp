43366d2e230eaa3400130c5bc9e5cfabecb635eb
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-16 19:54:17 -0300

    chore: add @routing path alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@routing": path.resolve(__dirname, "./src/routing"),
     },
   },
 });
