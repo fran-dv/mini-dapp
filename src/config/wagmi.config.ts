7a4d1cc9823dd76d40ff61c3342b5f1f69356296
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-17 19:58:06 -0300

    refactor(config): setup wagmi declaration merging to enforce type-safety

--- src/config/wagmi.config.ts
+++ src/config/wagmi.config.ts
@@ -1,6 +1,5 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import type { _chains } from "node_modules/@rainbow-me/rainbowkit/dist/config/getDefaultConfig";
-import type { Config } from "wagmi";
 import { sepolia } from "wagmi/chains";
 
 const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID ?? undefined;
@@ -8,8 +7,14 @@ const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID ?? undefined;
 export const supportedChains = [sepolia];
 export const defaultChain = sepolia;
 
-export const config: Config = getDefaultConfig({
-  appName: "My RainbowKit App",
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
+
+export const config = getDefaultConfig({
+  appName: "Wonderland challenge",
   projectId: projectId,
   chains: supportedChains as unknown as _chains,
   ssr: false,
