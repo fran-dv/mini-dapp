87798b7570dc43f2c743d6455e986e352f944548
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-16 22:23:44 -0300

    feat: implement Web3Providers component

---
+++ src/providers/Web3Providers.tsx
@@ -0,0 +1,22 @@
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { config } from "@config/wagmi.config";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+export const Web3Providers: React.FC<Props> = ({ children }: Props) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default Web3Providers;
