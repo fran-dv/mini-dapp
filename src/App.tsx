c8897ec31b6bb7ca4b95a8f1df507af76362f921
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-16 22:24:28 -0300

    feat: wrap App with Web3Providers component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { Route, Navigate } from "react-router-dom";
-import { Home } from "@pages/Home";
 import { AppLayout } from "@components/AppLayout";
+import "@rainbow-me/rainbowkit/styles.css";
+import Web3Providers from "./providers/Web3Providers";
+import { Home } from "@pages/Home";
 import { Paths } from "@routing/paths";
 import { RoutesWithNotFound } from "@routing/RoutesWithNotFound";
+import { Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <AppLayout>
-        <RoutesWithNotFound>
-          <Route path="/" element={<Navigate to={Paths.Home} />} />
-          <Route path={Paths.Home} element={<Home />} />
-        </RoutesWithNotFound>
-      </AppLayout>
+      <Web3Providers>
+        <AppLayout>
+          <RoutesWithNotFound>
+            <Route path="/" element={<Navigate to={Paths.Home} />} />
+            <Route path={Paths.Home} element={<Home />} />
+          </RoutesWithNotFound>
+        </AppLayout>
+      </Web3Providers>
     </div>
   );
 }
