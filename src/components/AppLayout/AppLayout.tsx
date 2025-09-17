6425f8d5546268f4b984456c5d4282d66a502a63
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-17 14:32:20 -0300

    feat: add Navbar to the AppLayout

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,9 +1,16 @@
+import { Navbar } from "@components/Navbar";
+
 interface Props {
   children: React.ReactNode;
 }
 
 export const AppLayout: React.FC<Props> = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
 };
 
 export default AppLayout;
