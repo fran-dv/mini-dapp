fccb65ac10542835d406967a90054cd870b1475c
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-19 00:12:04 -0300

    feat(api): add fetchTokenMarketData function

---
+++ src/api/fetchTokenMarketData.ts
@@ -0,0 +1,35 @@
+import type { TokenMarketData } from "@models/tokens";
+
+const COINGECKO_API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
+
+export const fetchTokenMarketData = async (coinGeckoId: string) => {
+  const endpoint = `https://api.coingecko.com/api/v3/simple/price?ids=${coinGeckoId}&vs_currencies=usd&include_24hr_change=true`;
+
+  try {
+    const response = await fetch(endpoint, {
+      headers: {
+        "x-cg-demo-api-key": COINGECKO_API_KEY,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error. Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const parsedData: TokenMarketData = {
+      coinGeckoId,
+      price: data[coinGeckoId].usd ?? 0,
+      priceChange24h: data[coinGeckoId].usd_24h_change ?? 0,
+    };
+
+    return parsedData;
+  } catch (err) {
+    console.error("Error fetching token market data: ", err);
+
+    throw err;
+  }
+};
+
+export default fetchTokenMarketData;
