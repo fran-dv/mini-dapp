93076e59a16b6da1e6c8039a8bdbf91d6ce54c81
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-23 20:17:22 -0300

    feat(Navbar): add Events link and disable buttons when isFormActionPending is true

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,16 +2,40 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import { Paths } from "@routing/paths";
+import useTokensStore from "@stores/tokens/useTokensStore";
 
 export const Navbar: React.FC = () => {
+  const { isFormActionPending } = useTokensStore();
   return (
     <header className={styles.wrapper}>
       <nav className={styles.container}>
         <div className={styles.navLinks}>
-          <button className={styles.linkButton}>
-            <Link className={styles.link} to={Paths.Home}>
-              Home
-            </Link>
+          <button className={styles.linkButton} disabled={isFormActionPending}>
+            {isFormActionPending ? (
+              <p className={styles.link}>Home</p>
+            ) : (
+              <Link
+                className={styles.link}
+                to={Paths.Home}
+                aria-disabled={isFormActionPending}
+              >
+                Home
+              </Link>
+            )}
+          </button>
+
+          <button className={styles.linkButton} disabled={isFormActionPending}>
+            {isFormActionPending ? (
+              <p className={styles.link}>Events</p>
+            ) : (
+              <Link
+                className={styles.link}
+                to={Paths.Events}
+                aria-disabled={isFormActionPending}
+              >
+                Events
+              </Link>
+            )}
           </button>
         </div>
         <ConnectButton showBalance={false} />
