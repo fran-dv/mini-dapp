34f9ce976e6341cffcf652b1a0cc9582ebafabda
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-24 01:15:47 -0300

    feat(NotFound): add basic 404 page design

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,20 @@
+import notFoundImage from "@assets/404.webp";
+import styles from "./NotFound.module.css";
+import { Link } from "react-router-dom";
+import Paths from "@routing/paths";
+
 export const NotFound: React.FC = () => {
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
+    <div className={styles.container}>
+      <img className={styles.image} src={notFoundImage} alt="404" />
+      <div className={styles.textContainer}>
+        <p>
+          You are more lost than the FIAT system. Return to{" "}
+          <Link className={styles.link} to={Paths.Home}>
+            wonderland
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
