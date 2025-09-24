ded45f356c79146a0e40192f22906c174197642c
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-24 01:53:47 -0300

    chore(vite): add Node types and fix __dirname in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
